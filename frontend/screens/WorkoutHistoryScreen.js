acd5a0c795d2629d4ee1c5afe55b31ec36adc18a
AHCarl <ahcarlin@gmail.com>
2019-01-24 07:56:23 -0600

    WorkoutHistoryScreen state properly populated

--- frontend/screens/WorkoutHistoryScreen.js
+++ frontend/screens/WorkoutHistoryScreen.js
@@ -7,38 +7,51 @@ import { ListItem } from 'react-native-elements'
 class WorkoutHistoryScreen extends React.Component {
 
   state = {
-    workouts: this.props.workouts,
-    workoutDates: []
+    myWorkouts: []
   }
 
   componentDidMount() {
     this.myWorkoutDates()
   }
 
+  // myWorkoutDates = () => {
+  //   let wos = this.state.workouts 
+  //   wos.forEach(wo => {
+  //     this.state.workoutDates.push(wo["date"])
+  //   });
+  //   console.log(this.state.workoutDates)
+  // }
+
   myWorkoutDates = () => {
-    let wos = this.state.workouts 
+    let wos = this.props.workouts 
     wos.forEach(wo => {
-      this.state.workoutDates.push(wo["date"])
+      var oneWo = {}
+      var woArr = []
+      oneWo.workoutDate = wo["date"]
+      wo["exercisesToday"].forEach( eT => {
+        woArr.push(eT["exercise"])
+        oneWo.exercises = woArr
+      })
+      this.state.myWorkouts.push(oneWo)
     });
-    console.log(this.state.workoutDates)
+    console.log(this.state.myWorkouts)
   }
 
-  // keyExtractor = (item, index) => index
+  keyExtractor = (item, index) => index
 
-  // renderItem = () => (
-  //   <ListItem
-  //     title={item.name}
-  //     subtitle={item.subtitle}
-  //   />
-  // )
+  renderItem = () => (
+    <ListItem
+      title={item.name}
+      subtitle={item.subtitle}
+    />
+  )
 
   render () {
-    console.log(this.state.workouts)
 
     return (
       // <FlatList
       //   keyExtractor={this.keyExtractor}
-      //   data={this.props.workouts}
+      //   data={this.props.workoutDates}
       //   renderItem={this.renderItem}
       // />
       <View></View>
